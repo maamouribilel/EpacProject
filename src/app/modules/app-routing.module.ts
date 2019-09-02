04011597dea8a5a86bc474010de6413ef00cf6f0
Bilel <maamouri.bilel@gmail.com>
2019-09-02 20:20:26 +0100

    initial commit

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from '../components/mainComponents/dashboard/dashboard.component';
+import { OrdersComponent } from '../components/mainComponents/orders/orders.component';
+import { SchedulingComponent } from '../components/mainComponents/scheduling/scheduling.component';
+import { StationsComponent } from '../components/mainComponents/stations/stations.component';
+import { ShippingComponent } from '../components/mainComponents/shipping/shipping.component';
+import { ReportsComponent } from '../components/mainComponents/reports/reports.component';
+import { ConfigurationsComponent } from '../components/mainComponents/configurations/configurations.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'shcheduling', component: SchedulingComponent },
+  { path: 'stations', component: StationsComponent },
+  { path: 'shipping', component: ShippingComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'configurations', component: ConfigurationsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
